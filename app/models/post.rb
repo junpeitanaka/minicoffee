1b858de901a0d8eb28a95094471e16564131e572
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-10-16 10:47:45 +0900

    フロント実装&MVC設定

---
+++ app/models/post.rb
@@ -0,0 +1,27 @@
+class Post < ApplicationRecord
+
+
+  def index
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :genre_id, :)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   get 'posts/index'
   root to: "posts#index"
+
+  resources  :posts, only: [:new, :create]
+
 end
