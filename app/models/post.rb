9b7e29cafbddaa41865fbbec8cbb9f4916e36f28
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-10-18 12:52:39 +0900

    フロント実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,8 @@ has_one_attached :image
 
 
 validates :genre_id, numericality:{other_than:1}
+validates :bitterness_id, numericality:{other_than:1}
+validates :aroma_id, numericality:{other_than:1}
+validates :rate_id, numericality:{other_than:1}
+
 end
